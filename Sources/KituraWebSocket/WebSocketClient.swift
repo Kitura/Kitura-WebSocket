bba6f9b4dbb8ea2564d9d3ced0986003afb98595
Shmuel Kallner <kallner@il.ibm.com>
2016-11-01 18:28:58 +0200

    IBM-Swift/Kitura#33 Added code to send a single frame message and respond to a ping with a pong

--- Sources/KituraWebSocket/WebSocketClient.swift
+++ Sources/KituraWebSocket/WebSocketClient.swift
@@ -14,10 +14,20 @@
  * limitations under the License.
  */
 
+import Dispatch
 import Foundation
 
 public class WebSocketClient {
     weak var processor: WSSocketProcessor?
+    
+    private static let bufferSize = 2000
+    private let buffer: NSMutableData
+    
+    private var writeLock = DispatchSemaphore(value: 1)
+    
+    init() {
+        buffer = NSMutableData(capacity: WebSocketClient.bufferSize) ?? NSMutableData()
+    }
     
     func received(frame: WSFrame) {
         
@@ -28,5 +38,57 @@ public class WebSocketClient {
         frame.payload.append(&zero, length: 1)
         print("WebSocketClient: payload=\(String(cString: frame.payload.bytes.assumingMemoryBound(to: CChar.self), encoding: .utf8))")
         frame.payload.length -= 1
+        
+        switch frame.opCode {
+        case .binary:
+            break
+            
+        case .close:
+            break
+            
+        case .continuation:
+            break
+            
+        case .ping:
+            sendMessage(withOpCode: .pong, payload: frame.payload)
+            
+        case .pong:
+            break
+            
+        case .text:
+            break
+            
+        case .unknown:
+            break
+        }
+    }
+    
+    private func sendMessage(withOpCode: WSFrame.FrameOpcode, payload: NSData) {
+        // Need to add logging
+        guard let processor = processor else { return }
+        
+        lockWriteLock()
+        
+        buffer.length = 0
+        WSFrame.createFrameHeader(finalFrame: true, opCode: withOpCode, payloadLength: payload.length, buffer: buffer)
+        
+        if WebSocketClient.bufferSize <= buffer.length + payload.length {
+            buffer.append(payload.bytes, length: payload.length)
+            processor.write(from: buffer)
+        }
+        else {
+            processor.write(from: buffer)
+            processor.write(from: payload)
+        }
+        
+        unlockWriteLock()
+    }
+    
+    private func lockWriteLock() {
+        _ = writeLock.wait(timeout: DispatchTime.distantFuture)
+    }
+    
+    private func unlockWriteLock() {
+        writeLock.signal()
     }
 }
