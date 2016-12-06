ac61beb73cda429fea55ad1a5575e731834e9dcf
Shmuel Kallner <kallner@il.ibm.com>
2016-12-06 19:28:10 +0200

    IBM-Swift/Kitura#33 Added helper functions for testing and updated exception description as in other repos

--- Tests/KituraWebSocketTests/KituraTest.swift
+++ Tests/KituraWebSocketTests/KituraTest.swift
@@ -16,13 +16,15 @@
 
 import XCTest
 
+@testable import KituraWebSocket
 @testable import KituraNet
+import Socket
 
 import Foundation
 import Dispatch
 
 protocol KituraTest {
-    func expectation(_ index: Int) -> XCTestExpectation
+    func expectation(line: Int, index: Int) -> XCTestExpectation
     func waitExpectation(timeout t: TimeInterval, handler: XCWaitCompletionHandler?)
 }
 
@@ -33,10 +35,10 @@ extension KituraTest {
     }
     
     func doTearDown() {
-        // sleep(10)
+        ConnectionUpgrader.clear()
     }
     
-    func performServerTest(_ router: ServerDelegate,
+    func performServerTest(_ router: ServerDelegate, line: Int = #line,
                            asyncTasks: @escaping (XCTestExpectation) -> Void...) {
         let server = HTTP.createServer()
         server.delegate = router
@@ -47,7 +49,7 @@ extension KituraTest {
             let requestQueue = DispatchQueue(label: "Request queue")
         
             for (index, asyncTask) in asyncTasks.enumerated() {
-                let expectation = self.expectation(index)
+                let expectation = self.expectation(line: line, index: index)
                 requestQueue.async() {
                     asyncTask(expectation)
                 }
@@ -63,12 +65,79 @@ extension KituraTest {
             XCTFail("Test failed. Error=\(error)")
         }
     }
+    
+    func sendUpgradeRequest(forProtocolVersion: String?, toPath: String, usingKey: String?) -> Socket? {
+        var socket: Socket?
+        do {
+            socket = try Socket.create()
+            try socket?.connect(to: "localhost", port: 8090)
+            
+            var request = "GET " + toPath + " HTTP/1.1\r\n" +
+                "Host: localhost:8090\r\n" +
+                "Upgrade: websocket\r\n" +
+                "Connection: Upgrade\r\n"
+            
+            if let protocolVersion = forProtocolVersion {
+                request += "Sec-WebSocket-Version: " + protocolVersion + "\r\n"
+            }
+            if let key = usingKey {
+                request += "Sec-WebSocket-Key: " + key + "\r\n"
+            }
+            
+            request += "\r\n"
+            
+            guard let data = request.data(using: .utf8) else { return nil }
+            
+            try socket?.write(from: data)
+        }
+        catch let error {
+            socket = nil
+            XCTFail("Failed to send upgrade request. Error=\(error)")
+        }
+        return socket
+    }
+    
+    func processUpgradeResponse(socket: Socket) -> (HTTPIncomingMessage?, NSData?) {
+        let response: HTTPIncomingMessage = HTTPIncomingMessage(isRequest: false)
+        var unparsedData: NSData?
+        var errorFlag = false
+        
+        var keepProcessing = true
+        let buffer = NSMutableData()
+        
+        do {
+            while keepProcessing {
+                buffer.length = 0
+                let count = try socket.read(into: buffer)
+                
+                if count != 0 {
+                    let parserStatus = response.parse(buffer)
+                    
+                    if parserStatus.state == .messageComplete {
+                        keepProcessing = false
+                        if parserStatus.bytesLeft != 0 {
+                            unparsedData = NSData(bytes: buffer.bytes+buffer.length-parserStatus.bytesLeft, length: parserStatus.bytesLeft)
+                        }
+                    }
+                }
+                else {
+                    keepProcessing = false
+                    errorFlag = true
+                    XCTFail("Server closed socket prematurely")
+                }
+            }
+        }
+        catch let error {
+            errorFlag = true
+            XCTFail("Failed to send upgrade request. Error=\(error)")
+        }
+        return (errorFlag ? nil : response, unparsedData)
+    }
 }
 
 extension XCTestCase: KituraTest {
-    func expectation(_ index: Int) -> XCTestExpectation {
-        let expectationDescription = "\(type(of: self))-\(index)"
-        return self.expectation(description: expectationDescription)
+    func expectation(line: Int, index: Int) -> XCTestExpectation {
+        return self.expectation(description: "\(type(of: self)):\(line)[\(index)]")
     }
     
     func waitExpectation(timeout t: TimeInterval, handler: XCWaitCompletionHandler?) {
