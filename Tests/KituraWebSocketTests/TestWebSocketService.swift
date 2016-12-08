b93bbdabe79fa5d8e22411300a3d734c226777ce
Shmuel Kallner <kallner@il.ibm.com>
2016-12-08 16:44:55 +0200

    IBM-Swift/Kitura#33 A WebSocketService to be used in the tests

---
+++ Tests/KituraWebSocketTests/TestWebSocketService.swift
@@ -0,0 +1,58 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import XCTest
+import Foundation
+
+@testable import KituraWebSocket
+
+class TestWebSocketService: WebSocketService {
+    var clientId = ""
+    let closeReason: WebSocketCloseReasonCode
+    
+    public init(closeReason: WebSocketCloseReasonCode) {
+        self.closeReason = closeReason
+    }
+    
+    public func connected(client: WebSocketClient) {
+        clientId = client.id
+    }
+    
+    public func disconnected(client: WebSocketClient, reason: WebSocketCloseReasonCode) {
+        XCTAssertEqual(clientId, client.id, "Client ID from connect wasn't client ID from disconnect")
+        XCTAssertEqual(Int(closeReason.code()), Int(reason.code()), "Excpected close reason code of \(closeReason) received \(reason)")
+    }
+    
+    public func received(message: Data, from: WebSocketClient) {
+        print("Received a binary message of length \(message.count)")
+        from.send(message: message)
+    }
+    
+    public func received(message: String, from: WebSocketClient) {
+        print("Received a String message of length \(message.characters.count)")
+        from.send(message: message)
+        
+        if message == "close" {
+            from.close(reason: .goingAway, description: "Going away...")
+        }
+        else if message == "drop" {
+            from.drop(reason: .policyViolation, description: "Droping...")
+        }
+        else if message == "ping" {
+            from.ping(withMessage: "Hello")
+        }
+    }
+}
