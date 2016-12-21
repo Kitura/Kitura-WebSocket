804e0b6b528e31a7221c6a07f4995c4a2a8b1848
Shmuel Kallner <kallner@il.ibm.com>
2016-12-21 11:23:41 +0200

    IBM-Swift/Kitura#33 Upgraded to KituraNet 1.4

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-WebSocket",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 8)
     ]
 )
