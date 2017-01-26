e8d89dd4febf75578935356aaded7384bca50c30
Navneet Gupta <navneet@us.ibm.com>
2017-01-26 17:00:19 -0600

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-WebSocket",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 8)
     ]
 )
