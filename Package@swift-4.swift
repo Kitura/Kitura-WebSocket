e5b88072e969d5afb0cef0db426ee93a3cbc8cfe
Quan Vo <quan.vo@ibm.com>
2017-09-25 11:43:45 -0500

    Fix KituraNet dependency name

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -39,7 +39,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KituraWebSocket",
-            dependencies: ["Kitura-net", "Cryptor"]),
+            dependencies: ["KituraNet", "Cryptor"]),
         .testTarget(
             name: "KituraWebSocketTests",
             dependencies: ["KituraWebSocket"]),
