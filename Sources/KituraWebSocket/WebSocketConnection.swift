6243a99dd035485258d5e0fecf334d085fe20321
Nethra Ravindran <nethra.ravindran@in.ibm.com>
2018-08-01 17:09:41 +0530

    Fix failures with continuation frames

--- Sources/KituraWebSocket/WebSocketConnection.swift
+++ Sources/KituraWebSocket/WebSocketConnection.swift
@@ -156,20 +156,22 @@ extension WebSocketConnection: ChannelInboundHandler {
                 message.write(buffer: &buffer)
                 if frame.fin {
                     switch messageState {
-                    case .binary:
-                        fireReceivedData(data: frame.data.getData(at: 0, length: frame.data.readableBytes) ?? Data())
-                    case .text:
-                        if let text = frame.unmaskedData.getString(at: 0, length: frame.unmaskedData.readableBytes, encoding: .utf8) {
-                            fireReceivedString(message: text)
-                        } else {
-                            connectionClosed(reason: .dataInconsistentWithMessage, description: "Failed to convert received payload to UTF-8 String")
-                        }
-                    case .unknown: //not possible
-                        break
+                        case .binary:
+                            fireReceivedData(data: message.getData(at: 0, length: message.readableBytes) ?? Data())
+                        case .text:
+                            if let data = message {
+                                guard let text = data.getString(at: 0, length: data.readableBytes, encoding: .utf8) else {
+                                    connectionClosed(reason: .dataInconsistentWithMessage, description: "Failed to convert received payload to UTF-8 String")
+                                    return
+                                }
+                                fireReceivedString(message: text)
+                            }
+                        case .unknown: //not possible
+                            break
                     }
                     messageState = .unknown
                 }
-                
+
             case .connectionClose:
                 if active {
                     let reasonCode: WebSocketErrorCode
