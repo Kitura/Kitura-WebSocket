9796b9e6f245fe44c314bdf8581bf202d386ae6c
Nethra Ravindran <nethra.ravindran@in.ibm.com>
2018-08-01 11:20:49 +0530

    Close connection for invalid opcode

--- Sources/KituraWebSocket/WebSocketConnection.swift
+++ Sources/KituraWebSocket/WebSocketConnection.swift
@@ -188,8 +188,11 @@ extension WebSocketConnection: ChannelInboundHandler {
             case .pong:
                 break
  
-            default:
-                break
+            case .unknownNonControl(let code):
+                closeConnection(reason: .protocolError, description: "Parsed frame with an invalid operation code of \(code)", hard: true)
+
+            case .unknownControl(let code):
+                closeConnection(reason: .protocolError, description: "Parsed frame with an invalid operation code of \(code)", hard: true)
 
         } 
     }
