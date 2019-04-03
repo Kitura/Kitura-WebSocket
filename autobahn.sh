011a5572222249da0107f2798931cd5f4bcbe344
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2019-04-03 13:59:15 +0530

    test: Run from 6-10

--- autobahn.sh
+++ autobahn.sh
@@ -24,7 +24,7 @@ FUZZING_CLIENT_JSON="{ \
          \"url\": \"ws://127.0.0.1:9001\" \
       } \
    ], \
-   \"cases\": [\"1.*\", \"2.*\", \"3.*\", \"4.*\", \"5.*\"], \
+   \"cases\": [\"6.*\", \"7.*\", \"8.*\", \"9.*\", \"10.*\"], \
    \"exclude-cases\": [], \
    \"exclude-agent-cases\": {} \
 }"
@@ -35,7 +35,7 @@ echo $FUZZING_CLIENT_JSON > fuzzingclient.json
 wstest -m fuzzingclient
 
 # Check if all tests passed
-OUTPUT = `grep behavior reports/servers/index.json | cut -d':' -f2 | cut -d'"' -f2 | sort -u | xargs`
+OUTPUT=`grep behavior reports/servers/index.json | cut -d':' -f2 | cut -d'"' -f2 | sort -u | xargs`
 
 echo "Behaviors output by tests: $OUTPUT"
 
